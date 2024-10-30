937a8608fbe9dfd1f68f77de2f5874a48aaf59a8
Priya12234 <134265065+Priya12234@users.noreply.github.com>
2024-10-30 11:01:03 +0530

    added isha as admin for admin screens

--- lib/Screens/fee_details_screen.dart
+++ lib/Screens/fee_details_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:hms/Screens/header_widget.dart'; // Import your HeaderWidget
+import 'package:hms/Screens/header_widget.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'razorpay_payment.dart'; // Import the RazorpayPayment class
+import 'razorpay_payment.dart';
 
 class FeeDetails extends StatefulWidget {
   const FeeDetails({super.key});
@@ -12,40 +12,35 @@ class FeeDetails extends StatefulWidget {
 }
 
 class _FeeDetailsState extends State<FeeDetails> {
-  String? userEmail; // Variable to hold the user email
-  late RazorpayPayment _razorpayPayment;
+  String? userEmail;
+  RazorpayPayment? _razorpayPayment;
 
   @override
   void initState() {
     super.initState();
-    _getUserEmail(); // Fetch the user email when the widget initializes
+    _getUserEmail();
   }
 
   @override
   void dispose() {
-    _razorpayPayment.dispose(); // Dispose Razorpay resources when done
+    _razorpayPayment?.dispose();
     super.dispose();
   }
 
-  // Function to get the user email based on their ID from Firestore
   Future<void> _getUserEmail() async {
     final prefs = await SharedPreferences.getInstance();
-    final userId =
-        prefs.getString('userId'); // Fetch the user ID from SharedPreferences
+    final userId = prefs.getString('userId');
 
     if (userId != null) {
-      // Fetch the user email from Firestore based on the user ID
       DocumentSnapshot userDoc = await FirebaseFirestore.instance
-          .collection('users') // Adjust to your user collection
+          .collection('users')
           .doc(userId)
           .get();
 
       if (userDoc.exists) {
         setState(() {
-          userEmail =
-              userDoc['email']; // Assuming the email field is named 'email'
-          _razorpayPayment = RazorpayPayment(
-              userEmail); // Initialize RazorpayPayment with user email
+          userEmail = userDoc['email'];
+          _razorpayPayment = RazorpayPayment(userEmail); // Initialize after email is set
         });
       }
     }
@@ -56,19 +51,17 @@ class _FeeDetailsState extends State<FeeDetails> {
     return Scaffold(
       body: Column(
         children: [
-          const HeaderWidget(
-              title: 'Fees Details'), // Add the HeaderWidget here
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Expanded(
+          const HeaderWidget(title: 'Fees Details'),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
               child: StreamBuilder<QuerySnapshot>(
                 stream: userEmail != null
                     ? FirebaseFirestore.instance
                         .collection('admin_fees_details')
-                        .where('email', isEqualTo: userEmail) // Filter by email
+                        .where('email', isEqualTo: userEmail)
                         .snapshots()
-                    : const Stream
-                        .empty(), // Return an empty stream if userEmail is not set
+                    : const Stream.empty(),
                 builder: (context, snapshot) {
                   if (snapshot.connectionState == ConnectionState.waiting) {
                     return const Center(child: CircularProgressIndicator());
@@ -80,60 +73,48 @@ class _FeeDetailsState extends State<FeeDetails> {
 
                   final documents = snapshot.data!.docs;
 
-                  return SingleChildScrollView(
-                    child: Column(
-                      children: documents.map<Widget>((doc) {
-                        final data = doc.data() as Map<String, dynamic>;
-                        final String status = data['status'];
-                        final bool isPaid = status == 'paid';
+                  return ListView.builder(
+                    itemCount: documents.length,
+                    itemBuilder: (context, index) {
+                      final data = documents[index].data() as Map<String, dynamic>;
+                      final String status = data['status'];
+                      final bool isPaid = status == 'paid';
 
-                        return Card(
-                          elevation: 4,
-                          margin: const EdgeInsets.symmetric(
-                              vertical: 8, horizontal: 4),
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('Email: ${data['email']}',
-                                    style: const TextStyle(fontSize: 18)),
-                                Text('Building: ${data['building']}',
-                                    style: const TextStyle(fontSize: 16)),
-                                Text('Type: ${data['type']}',
-                                    style: const TextStyle(fontSize: 16)),
-                                Text('Fees: ₹${data['fees'].toString()}',
-                                    style: const TextStyle(fontSize: 16)),
-                                Text('Status: ${isPaid ? 'Paid' : 'Unpaid'}',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: isPaid ? Colors.green : Colors.red,
-                                    )),
-                                Text('Date: ${data['date']}',
-                                    style: const TextStyle(fontSize: 16)),
-                                const SizedBox(height: 10),
-                                ElevatedButton(
-                                  onPressed: isPaid
-                                      ? null
-                                      : () => _razorpayPayment.openCheckout(
-                                          context,
-                                          doc.id), // Pass the context here
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: isPaid
-                                        ? Colors.grey
-                                        : Colors
-                                            .blue, // Change color based on payment status
-                                  ),
-                                  child: Text(isPaid ? 'Paid' : 'Pay Now',
-                                      style:
-                                          const TextStyle(color: Colors.white)),
+                      return Card(
+                        elevation: 4,
+                        margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Email: ${data['email']}', style: const TextStyle(fontSize: 18)),
+                              Text('Building: ${data['building']}', style: const TextStyle(fontSize: 16)),
+                              Text('Type: ${data['type']}', style: const TextStyle(fontSize: 16)),
+                              Text('Fees: ₹${data['fees'].toString()}', style: const TextStyle(fontSize: 16)),
+                              Text('Status: ${isPaid ? 'Paid' : 'Unpaid'}',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: isPaid ? Colors.green : Colors.red,
+                                  )),
+                              Text('Date: ${data['date']}', style: const TextStyle(fontSize: 16)),
+                              const SizedBox(height: 10),
+                              ElevatedButton(
+                                onPressed: isPaid
+                                    ? null
+                                    : () => _razorpayPayment?.openCheckout(
+                                        context, documents[index].id),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: isPaid ? Colors.grey : Colors.blue,
                                 ),
-                              ],
-                            ),
+                                child: Text(isPaid ? 'Paid' : 'Pay Now',
+                                    style: const TextStyle(color: Colors.white)),
+                              ),
+                            ],
                           ),
-                        );
-                      }).toList(),
-                    ),
+                        ),
+                      );
+                    },
                   );
                 },
               ),
